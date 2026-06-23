a7fe37f0a0595453b2a63806f17f19d02964933b
dpsong <57675424+dpsong@users.noreply.github.com>
2026-06-23 18:30:53 +0800

    Update data_root path for OCTVFDataset

--- drb/datasets/octvf_dataset.py
+++ drb/datasets/octvf_dataset.py
@@ -183,7 +183,7 @@ class OCTVFDataset(Base):
 
 
 if __name__ == '__main__':
-    data_root = '/cpfs01/user/songdiping/Workspaces/Data/oct2vf/zoc3d-2022_resize384/'
+    data_root = 'oct2vf/zoc3d-2022/'
     split = 'TrainVal_split/macula_val_split.json'
     test_dataset = OCTVFDataset(data_root, split, num_classes=5)
     print(test_dataset.__len__())
